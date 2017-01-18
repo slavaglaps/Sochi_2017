6bbffcd3329a039605ba2fcca1cda2766a954023
Alexander Zimin <azimin@me.com>
2017-01-18 20:00:45 +0300

    base project init

---
+++ CWWG/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CWWG
+//
+//  Created by Alexander Zimin on 18/01/2017.
+//  Copyright © 2017 CWWG Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
